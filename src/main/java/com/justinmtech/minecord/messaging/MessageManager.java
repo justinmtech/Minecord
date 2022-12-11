3797e00e3edce6a99cad3bb16126aabee83243b9
BuildTools <unconfigured@null.spigotmc.org>
2022-12-11 14:38:46 -0600

    Added documentation

--- src/main/java/com/justinmtech/minecord/messaging/MessageManager.java
+++ src/main/java/com/justinmtech/minecord/messaging/MessageManager.java
@@ -8,9 +8,14 @@ import org.bukkit.entity.Player;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.MessageBuilder;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.Optional;
 
-//Deliver notifications to both the Discord and Minecraft server
+/**
+ * Deliver notifications to both the Discord and Minecraft server
+ */
 public class MessageManager {
     private final TextChannel textChannel;
 
@@ -18,7 +23,13 @@ public class MessageManager {
         this.textChannel = textChannel;
     }
 
-    public void sendMessage(String message, MessageDestination dest, NotifyType type, Sound sound) {
+    /**
+     * @param message Message to send
+     * @param dest Destination server(s)
+     * @param type Message type
+     * @param sound Bukkit Sound
+     */
+    public void sendMessage(@NotNull String message, @NotNull MessageDestination dest, @NotNull NotifyType type, @NotNull Sound sound) {
         if (dest.equals(MessageDestination.MINECRAFT)) {
             sendMinecraftNotification(message, type, sound);
         } else if (dest.equals(MessageDestination.DISCORD)) {
@@ -29,7 +40,12 @@ public class MessageManager {
         }
     }
 
-    public void sendMessage(String message, MessageDestination dest, NotifyType type) {
+    /**
+     * @param message Message to send
+     * @param dest Destination server(s)
+     * @param type Message type
+     */
+    public void sendMessage(@NotNull String message, @NotNull MessageDestination dest, @NotNull NotifyType type) {
         if (dest.equals(MessageDestination.MINECRAFT)) {
             sendMinecraftNotification(message, type, null);
         } else if (dest.equals(MessageDestination.DISCORD)) {
@@ -40,7 +56,12 @@ public class MessageManager {
         }
     }
 
-    public void sendMinecraftNotification(String message, NotifyType type, Sound sound) {
+    /**
+     * @param message Message to send
+     * @param type Message type
+     * @param sound Bukkit Sound
+     */
+    public void sendMinecraftNotification(@NotNull String message, @NotNull NotifyType type, @Nullable Sound sound) {
         if (type.equals(NotifyType.NORMAL)) {
             Bukkit.broadcastMessage(message);
         } else {
@@ -51,14 +72,21 @@ public class MessageManager {
         }
     }
 
-    public void sendMinecraftSoundToOnlinePlayers(Sound sound) {
+    /**
+     * @param sound Bukkit Sound
+     */
+    public void sendMinecraftSoundToOnlinePlayers(@Nullable Sound sound) {
         if (sound == null) return;
         for (Player player : Bukkit.getOnlinePlayers()) {
             player.playSound(player.getLocation(), sound, 1, 1);
         }
     }
 
-    public void sendDiscordMessage(String message, NotifyType type) {
+    /**
+     * @param message Message to send
+     * @param type Message type
+     */
+    public void sendDiscordMessage(@NotNull String message, @NotNull NotifyType type) {
         String messageStripped = ChatColor.stripColor(message);
         if (type.equals(NotifyType.NORMAL)) {
             sendDiscordTextNotification(messageStripped);
@@ -67,19 +95,31 @@ public class MessageManager {
         }
     }
 
-    public void sendDiscordTextNotification(String message) {
+    /**
+     * @param message Message to send
+     */
+    public void sendDiscordTextNotification(@NotNull String message) {
         new MessageBuilder()
                 .append(message)
                 .send(getTextChannel());
     }
 
-    public void sendDiscordCodeNotification(String message) {
+    /**
+     * @param message Message to send
+     */
+    public void sendDiscordCodeNotification(@NotNull String message) {
         new MessageBuilder()
                 .appendCode("", message)
                 .send(getTextChannel());
     }
 
-    public static TextChannel getTextChannel(DiscordApi discordApi, String channelId) throws ChannelNotFoundException {
+    /**
+     * @param discordApi Discord api object
+     * @param channelId A String of numbers (right click a channel in developer mode and copy ID to get it)
+     * @return TextChannel
+     * @throws ChannelNotFoundException Throw ChannelNotFoundException with message
+     */
+    public static TextChannel getTextChannel(@NotNull DiscordApi discordApi, @NotNull String channelId) throws ChannelNotFoundException {
         if (channelId.equals("")) throw new ChannelNotFoundException("You have the discord bot enabled, but the text channel is invalid.");
         Optional<TextChannel> textChannel = discordApi.getTextChannelById(channelId);
         if (textChannel.isEmpty()) throw new ChannelNotFoundException("You have the discord bot enabled, but the text channel is invalid.");
